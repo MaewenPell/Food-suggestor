860686970801639ba1fd3b3128aa54f40589ea43
Pelletier Maewen <maewen.p@gmail.com>
2020-02-04 15:52:06 +0100

    [Draft : organisation des requêtes]

--- get_categories.py
+++ get_categories.py
@@ -1,81 +1,95 @@
 import requests
 
 
-class Get_informations:
+class Get_informations():
     '''
         Class use to fetch data from
         OpenFoodFact and retrieve informations
     '''
-    def __init__(self):
-        # TODO : change with the wanted product instead of
-        #        the first one from all the products
-        #
-        self.manage_informations()
+    def __init__(self, categorie):
+        # TODO : Get the number of product into
+        # a conf.py
+        self.NB_PRODUCT = 3
+        self.manage_informations(categorie)
 
-    def manage_informations(self):
+    def manage_informations(self, categorie):
         '''
             Routine use to call the api and receive json data
             and triggerd the different functions
         '''
         r = requests.get(
-           "https://fr.openfoodfacts.org/categorie/boissons.json")
+            f"https://fr.openfoodfacts.org/categorie/{categorie}.json")
         r = r.json()
 
-        name = self.get_name(r)
-        categories = self.get_categories(r)
-        store = self.get_store(r)
-        link = self.get_link(r)
-        nutriscore = self.get_nutriscore(r)
+        for i in range(self.NB_PRODUCT):
+            try:
+                categories = self.get_categories(r, i)
+                name = self.get_name(r, i)
+                store = self.get_store(r, i)
+                link = self.get_link(r, i)
+                nutriscore = self.get_nutriscore(r, i)
 
-        print(f"""Categories are : {categories}
-                  Name : {name}
-                  Store : {store}
-                  Link : {link}
-                  Nutriscore : {nutriscore}""")
+                print(f"""Categories are : {categories}
+                        Name : {name}
+                        Store : {store}
+                        Link : {link}
+                        Nutriscore : {nutriscore}""")
+            except KeyError as e:
+                print("The following key is not referenced :\n",
+                      e)
 
-    def get_categories(self, data_to_fetch):
+    def get_categories(self, data_to_fetch, current_prod):
         '''
             Function used to trigger all the categories
         '''
-        first_product = (data_to_fetch['products'][1])
-        category = first_product['categories']
+        current_product = (data_to_fetch['products'][current_prod])
+        category = current_product['categories']
+        print(category)
         return category
 
-    def get_name(self, data_to_fetch):
+    def get_name(self, data_to_fetch, current_prod):
         '''
             Function used to retrieve the name from the
             data
         '''
-        get_elem = (data_to_fetch['products'][1])
-        name = get_elem['product_name_fr']
+        current_product = (data_to_fetch['products'][current_prod])
+        try:
+            name = current_product['product_name_fr']
+            name = current_product['generic_name_fr']
+        except KeyError:
+            name = "Not found"
         return name
 
-    def get_store(self, data_to_fetch):
+    def get_store(self, data_to_fetch, current_prod):
         '''
         Function use to get the store
         '''
-        first_product = data_to_fetch['products'][1]
-        store = first_product['stores']
+        current_product = (data_to_fetch['products'][current_prod])
+        store = current_product['stores']
         return store
 
-    def get_link(self, data_to_fetch):
+    def get_link(self, data_to_fetch, current_prod):
         '''
             Function used to get the link
             from the OPFF website
         '''
-        first_product = data_to_fetch['products'][1]
-        link = first_product['url']
+        current_product = (data_to_fetch['products'][current_prod])
+        link = current_product['url']
         return link
 
-    def get_nutriscore(self, data_to_fetch):
+    def get_nutriscore(self, data_to_fetch, current_prod):
         ''' Function used to get the nutriscore from
             the wanted product
         '''
-        first_product = data_to_fetch['products'][1]
-        nutriscore = first_product['nutrition_grade_fr']
+        current_product = data_to_fetch['products'][current_prod]
+        try:
+            nutriscore = current_product['nutrition_grade_fr']
+        except KeyError:
+            nutriscore = current_product['nutrition_grades_tags']
+
         return nutriscore
 
 
-if __name__ == "__main__":
-    current_search = Get_informations()
-    response = current_search.manage_informations()
+# if __name__ == "__main__":
+#     current_search = Get_informations()
+#     response = current_search.manage_informations()
---
+++ user_interaction.py
@@ -0,0 +1,68 @@
+import re
+from get_categories import Get_informations
+
+
+class main_windows():
+    def __init__(self):
+        self.valid_choice = False
+        self.main_prog()
+
+    def main_prog(self):
+        '''
+            Functon used to handle the general
+            execution of the program
+        '''
+        main_message = """
+         ---------------------------
+        | Welcome to Food Suggestor |
+         ---------------------------
+        """
+        print(main_message)
+        cat_choice = self.user_choice()
+        self.related_categories(cat_choice)
+
+    def user_choice(self):
+        '''
+        Function used to display the possible
+        category choice
+        '''
+        print("Please chose a category of product : ")
+        print("1 - Drinks")
+        print("2 - Biscuits and cakes")
+        print("3 - Spreads")
+        print("4 - Frozen food")
+        print("5 - Convenience food")
+        while not self.valid_choice:
+            try:
+                user_choice = int(input(re.sub(' +', ' ', """
+                              Please type the number
+                              of the wanted categorie : """)))
+                if user_choice <= 5 and user_choice > 0:
+                    self.valid_choice = True
+                else:
+                    print("Please choice among the proposed values : ")
+            except ValueError:
+                print("Please choice among the proposed values : ")
+        return user_choice
+
+    def related_categories(self, user_choice):
+        '''
+            Function used to make the correspondance
+            between the user choice and the category
+        '''
+        # TODO : Link propre avec BDD ici --> ID
+        if user_choice == 1:
+            Get_informations("boissons")
+        elif user_choice == 2:
+            Get_informations("biscuit-et-gateaux")
+        elif user_choice == 3:
+            Get_informations("produits-a-tartiner")
+        elif user_choice == 4:
+            Get_informations("surgeles")
+        elif user_choice == 5:
+            Get_informations("plats-prepares")
+
+
+
+if __name__ == "__main__":
+    main_windows = main_windows()
